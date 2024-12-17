ae72750e08a2ea45dc1bbf3fa3d602a3bbd029fc
vednav9 <vedantnavthale222@nhitm.ac.in>
2024-12-17 19:16:32 +0530

    math

--- MathDSA/Prac.java
+++ MathDSA/Prac.java
@@ -4,10 +4,27 @@ import java.util.Arrays;
 
 public class Prac {
     public static void main(String[] args) {
-        int a=3;
-        int b=6;
-        int ans=(a^b);
+        int a = 3;
+        int b = 6;
+        int ans = (a ^ b);
 
         System.out.println(ans);
+
+        // isOdd
+        int c=35234;
+        System.out.println(isOdd(c));
+
+        // Fint i th bit
+        int n=3855325;
+        int i =findIth(n);
+        //System.out.println(Integer.toBinaryString(i));
+    }
+
+    static boolean isOdd(int c){
+        return (c&1)==1;
+    }
+
+    static int findIth(int n){
+        return n & (1<<6);
     }
 }
