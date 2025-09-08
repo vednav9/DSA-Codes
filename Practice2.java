eba2b57b7dde1afd1d77ff399afba80d75ea0c7d
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-09-08 21:10:54 +0530

    Practice2

--- Practice2.java
+++ Practice2.java
@@ -1,8 +1,33 @@
+// import java.util.*;
+
 public class Practice2 {
+    // Reverse a string without using built-in functions.
+
     public static void main(String[] args) {
-        // count by while loop /10
-        // for
-        // convert to string the sue length
+        System.out.println(16&1);
         
+    }
+
+    public static boolean isPalindrome(String s) {
+        int i=0;
+        s=s.strip();
+        int j=s.length()-1;
+
+        while (i<j) {
+            if (s.charAt(i)==s.charAt(j)) {
+                i++;
+                j--;
+            }
+            else if ((s.charAt(i)==':') || (s.charAt(i)==',')) {
+                i++;
+            }
+            else if ((s.charAt(j)==':') || (s.charAt(j)==',')) {
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
