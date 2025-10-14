51ec30eb91f20f1f53604f19ffcee9da8cb8165f
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-14 14:11:07 +0530

    Bug Fix

--- OOP/Polymorphism.java
+++ OOP/Polymorphism.java
@@ -17,7 +17,7 @@ class Student {
     }
 }
 
-public class PolymorphismC {
+public class Polymorphism {
     public static void main(String[] args) {
         Student s1=new Student();
         s1.name="Vedant";
