b42ced5cfafd6129daad00d539f19dec6d32ca1f
vednav9 <vedantnavthale222@nhitm.ac.in>
2026-04-01 19:17:34 +0530

    Add CheeryPickup algorithm implementation and refactor MinPathSum for consistency

--- COMMON_PATTERNS/DP/DPonGrids/MinPathSum.java
+++ COMMON_PATTERNS/DP/DPonGrids/MinPathSum.java
@@ -2,25 +2,23 @@ package DP.DPonGrids;
 
 public class MinPathSum {
     public int minPathSum(int[][] grid) {
-        int n=grid.length, m=grid[0].length;
-        int[][] dp=new int[n][m];
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
 
-        dp[0][0]=grid[0][0];
+        dp[0][0] = grid[0][0];
 
-        for (int i = 1; i < n; i++) {
-            dp[i][0]=dp[i-1][0] + grid[i][0];
-        }
+        for (int i = 1; i < m; i++)
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
 
-        for (int j = 1; j < n; j++) {
-            dp[0][j]=dp[0][j-1] + grid[0][j];
-        }
+        for (int j = 1; j < n; j++)
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
 
-        for(int i=1;i<n;i++){
-            for (int j = 1; j < m; j++) {
-                dp[i][j]=grid[i][j]+ Math.min(grid[i-1][j], grid[i][j-1]);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] +
+                        Math.min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-
-        return dp[n-1][m-1];
+        return dp[m - 1][n - 1];
     }
 }
